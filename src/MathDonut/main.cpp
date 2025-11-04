eecd1ea929095238a2ba2024f0a2ac3b0c75a5c3
Noa <npascal@gaming.tech>
2025-11-04 09:02:29 +0100

    [REF] 1.2

--- src/MathDonut/main.cpp
+++ src/MathDonut/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <Windows.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     
+    // HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    // DWORD mode;
+    // GetConsoleMode(hConsole, &mode);
+    // SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
     
     std::cout << "Hello, World!\n";
     
