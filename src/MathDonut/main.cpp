e73cc2e167c92e394d2c4b0df0ac0cd6cdd4c334
Noa <npascal@gaming.tech>
2025-11-04 08:59:57 +0100

    [ADD] 1.2

--- src/MathDonut/main.cpp
+++ src/MathDonut/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-#include "main.h"
+#include <Windows.h>
 
 int main()
 {
+    
+    
     std::cout << "Hello, World!\n";
+    
+    std::cout << "\x1b[2J\n";
+    std::cout << "\x1b[H\n";
+    std::cout << "\x1b[?25l\n";
+
+    while (true)
+    {
+        
+    }
     return 0;
 }
