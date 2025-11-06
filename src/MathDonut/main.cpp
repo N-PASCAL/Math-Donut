8f8147c572b48a05930ddd0ae33b8edbee090a49
Noa <npascal@gaming.tech>
2025-11-06 08:54:42 +0100

    [ADD] 1.4

--- src/MathDonut/main.cpp
+++ src/MathDonut/main.cpp
@@ -9,11 +9,17 @@ int main(int argc, char *argv[])
     // GetConsoleMode(hConsole, &mode);
     // SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
     
-    std::cout << "\x1b[2J\n";
-    std::cout << "\x1b[H\n";
-    std::cout << "\x1b[?25l\n";
-    std::cout << "Hello, World!\n";
-    std::cout << "\x1b[?25h\n";
+    std::cout << "\x1b[2J\n";       // clear
+    std::cout << "\x1b[H\n";        // (0,0)
+    std::cout << "\x1b[?25l\n";     // cache le curseur
+    std::cout << "\x1b[?25h\n";     // montre le curseur
+
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 100; j++)
+            std::cout << ".";
+        std::cout << "\n";
+    }
     
     return 0;
 }
