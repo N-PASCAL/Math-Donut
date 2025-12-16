073bf981fc1813fefb17e253f243c972ae2727d1
Noa <147041489+PASCAL-Noa@users.noreply.github.com>
2025-12-16 08:22:29 +0100

    [ADD] 4.2

--- src/MathDonut/Mesh.h
+++ src/MathDonut/Mesh.h
@@ -15,9 +15,14 @@ struct Vertex
 {
     void Rotate(float angle, Axis axis);
     void Debug() const { std::printf("[x=%5.2f, y=%5.2f, z=%5.2f]\n", x, y, z); }
+    
     float x;
     float y;
     float z;
+
+    float nx;
+    float ny;
+    float nz;
 };
 
 class Mesh
